2976af7b6ddf9f8d9c279ff68fd792ee27c211b9
Altynbek <altynbekzhumadiluulu@gmail.com>
2022-05-23 17:20:38 +0600

    finish pr1

--- src/main/java/peaksoft/config/AppConfig.java
+++ src/main/java/peaksoft/config/AppConfig.java
@@ -1,9 +1,29 @@
 package peaksoft.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import peaksoft.model.Animal;
+import peaksoft.model.Cat;
+import peaksoft.model.Dog;
+import peaksoft.model.Timer;
 
 @Configuration
 @ComponentScan(basePackages = "peaksoft")
 public class AppConfig {
+//
+//    @Bean
+//    public Animal animal() {
+//        return new Cat();
+//    }
+//
+//    @Bean
+//    public Animal animal1() {
+//        return new Dog();
+//    }
+//
+//    @Bean
+//    public Timer timer() {
+//        return timer();
+//    }
 }
--- src/main/java/peaksoft/model/Animal.java
+++ src/main/java/peaksoft/model/Animal.java
@@ -1,4 +1,12 @@
 package peaksoft.model;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Animal {
+    @Bean
+    public Animal dog(){
+        return new Dog();
+    }
 }
